0727edb4d2275dd61d05b3ec8bf6f688cfbe87b0
Madulak <madula.kamanga@gmail.com>
2020-07-22 19:12:20 +0000

    Updates

---
+++ precache-manifest.eed06210ca343f7813cfb7285f267236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4ec1611d27c08939630380f740b6ec",
+    "url": "/instagramclone/index.html"
+  },
+  {
+    "revision": "fe84b266fe7cfec44099",
+    "url": "/instagramclone/static/css/main.b06d7f84.chunk.css"
+  },
+  {
+    "revision": "fa42620eceeafb54d037",
+    "url": "/instagramclone/static/js/2.637ee2af.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/instagramclone/static/js/2.637ee2af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe84b266fe7cfec44099",
+    "url": "/instagramclone/static/js/main.07475bb8.chunk.js"
+  },
+  {
+    "revision": "90b3ec0f292067cccb0d",
+    "url": "/instagramclone/static/js/runtime-main.9e3ca166.js"
+  }
+]);
